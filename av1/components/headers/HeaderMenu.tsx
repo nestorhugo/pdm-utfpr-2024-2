7e46eb58f7c599a2dd60ad9234c257335dd31a41
nestorhugo <nestorhugo@proton.me>
2024-11-21 01:15:00 -0300

    :sparkles: feat: add ActionSheet to HeaderMenu

--- av1/components/headers/HeaderMenu.tsx
+++ av1/components/headers/HeaderMenu.tsx
@@ -2,13 +2,44 @@ import { View, Text, TouchableOpacity, Alert, StyleSheet } from "react-native";
 import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { PRIMARY_COLOR } from "@/constants/theme";
+import { useActionSheet } from "@expo/react-native-action-sheet";
 
 export default function HeaderMenu() {
-  const handleMenuPress = () => {
-    Alert.alert("Menu Pressed", "This is where the menu would open.");
+  const { showActionSheetWithOptions } = useActionSheet();
+
+  const onPress = () => {
+    const options = ["Sobre", "Logout", "Cancelar"];
+    const destructiveButtonIndex = 1;
+    const cancelButtonIndex = 2;
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+        destructiveButtonIndex,
+      },
+      (selectedIndex?: number) => {
+        switch (selectedIndex) {
+          case 1:
+            // Save
+            //TODO -> lidar com os redirects do ActionSheet
+            console.log("Sobre");
+            break;
+
+          case destructiveButtonIndex:
+            // Delete
+            console.log("Deslogar");
+            break;
+
+          case cancelButtonIndex:
+          // Canceled
+        }
+      }
+    );
   };
+
   return (
-    <TouchableOpacity onPress={handleMenuPress} style={styles.menuButton}>
+    <TouchableOpacity onPress={onPress} style={styles.menuButton}>
       <MaterialIcons name="menu" size={24} color={PRIMARY_COLOR} />
     </TouchableOpacity>
   );
